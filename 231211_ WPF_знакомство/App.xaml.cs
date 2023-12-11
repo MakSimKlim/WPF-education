596adb8c3be73c76db2ae00d996f7131e6a1845e
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-12-11 21:56:49 +0500

    Add files via upload

---
+++ 231211_ WPF_знакомство/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Startup += AppStartUp();
+        }
+        private void AppStartUp(object sender, StartupEventArgs e)
+        { 
+        
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
---
+++ 231211_ WPF_знакомство/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //базовые свойства
+            InitializeComponent();
+            AllowsTransparency = true;
+            Icon;
+            IsActive;
+            Left;
+            Top;
+            Owner;
+            ResizeMode = ResizeMode.CanResize;
+            RestoreBounds;
+            ShowActivated;
+            ShowInTaskbar = true;// true только у главного окна
+            Topmost = true;//ПОВЕРХ ВСЕХ ОКОН   
+            WindowStartupLocation;
+            WindowState = WindowState.Normal;
+            Acivated;
+            Closed;
+            ContentRendered;//контент отображен
+            LocationChanged;
+            SourceInitialized;
+            StateChanged;
+            Activate();
+            Close();
+            Show();
+            ShowDialog();
+            SizeToContent.Height;// автоподстройка под высоту
+            WindowStartupLocation = WindowStartupLocation;
+            WindowStyle = WindowStyle.ToolWindow;//рамка
+
+
+        }
+    }
+}
